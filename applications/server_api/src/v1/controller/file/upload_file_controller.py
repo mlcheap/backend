8f36eb8cf5fa93b0d725be1ce97265f8693f1151
arash <arashsafari86@gmail.com>
2022-01-10 23:08:29 +0330

    make directory path

--- applications/server_api/src/v1/controller/file/upload_file_controller.py
+++ applications/server_api/src/v1/controller/file/upload_file_controller.py
@@ -1,6 +1,7 @@
 from model.file import save_file, insert_file_info, get_file_path, get_file_dir
 from model.customer import find_customer_by_token
 from werkzeug.utils import secure_filename
+import os
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'mp4'}
 
@@ -18,6 +19,7 @@ def upload_file_process(token, file):
         size = len(blob)
         file_name = secure_filename(file.filename)
         file_dir = get_file_dir(str(customer["_id"]))
+        os.makedirs(file_dir)
         created_at, updated_at, mime_type, file_id, file_name = \
             insert_file_info(str(customer["_id"]), file_dir, file_name, size)
         file_saved_name = str(file_id) + "." + file_name.rsplit('.', 1)[1].lower()
