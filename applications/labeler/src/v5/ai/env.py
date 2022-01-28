39c37df466e7359fbadb8177ce729178b4c01c12
arash <arashsafari86@gmail.com>
2022-01-28 10:09:02 +0330

    use model_url

--- applications/labeler/src/v5/ai/env.py
+++ applications/labeler/src/v5/ai/env.py
@@ -1,7 +1,7 @@
 import os
 
-# MODEL_BASE_URL = os.getenv('MODEL_URL') or "https://skillai.mlcheap.com"
-MODEL_BASE_URL = "https://skillai.mlcheap.com"
+MODEL_BASE_URL = os.getenv('MODEL_URL') or "https://skillai.mlcheap.com"
+# MODEL_BASE_URL = "https://skillai.mlcheap.com"
 # LABELER_BASE_URL = "http://localhost:6221"
 HTTP_TOTAL_RETRIES = 4  # Number of total retries
 HTTP_RETRY_BACKOFF_FACTOR = 1  # Wait 1, 2, 4 seconds between retries
